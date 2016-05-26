24f06f3f926c27de120f1f88800186c290920136
cuongpt105 <cuongpt105@gmail.com>
2016-05-26 18:25:58 +0700

    show log for connection

--- app.js
+++ app.js
@@ -4,7 +4,22 @@ var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
 
+//mongodb://<dbuser>:<dbpassword>@ds015953.mlab.com:15953/mongo-example-test
 mongoose.connect('mongodb://cuongmongolab105:cuongmongolab675214@ds015953.mlab.com:15953/mongo-example-test'); // connect to our database
+
+mongoose.connection.on('connected', function () {  
+  console.log('Mongoose default connection open to ' + dbURI);
+}); 
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {  
+  console.log('Mongoose default connection error: ' + err);
+}); 
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {  
+  console.log('Mongoose default connection disconnected'); 
+});
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
